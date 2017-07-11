d266ecd036387a4feaf5e8c36d054c61ad9bee64
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-07-11 12:42:20 -0500

    added inl and outl
    * for reading 32 bit values
    * needed for pci support

--- src/AsmHelper.h
+++ src/AsmHelper.h
@@ -20,15 +20,24 @@ static inline uint8_t inb(uint16_t port) {
 
 static inline void outb(uint16_t port, uint8_t val) {
 	asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
-	/* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
-	 * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
-	 * The  outb  %al, %dx  encoding is the only option for all other cases.
-	 * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+
+}
+
+static inline uint32_t inl(uint16_t port) {
+	uint8_t ret;
+	asm volatile ( "inl %1, %0"
+			: "=a"(ret)
+			: "Nd"(port) );
+	return ret;
+}
+
+static inline void outl(uint16_t port, uint32_t val) {
+	asm volatile ( "outl %0, %1" : : "a"(val), "Nd"(port) );
+
 }
 
 static inline void io_wait(void)
 {
-    /* TODO: This is probably fragile. */
     asm volatile ( "jmp 1f\n\t"
                    "1:jmp 2f\n\t"
                    "2:" );
