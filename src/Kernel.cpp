2a0eceb4a09b9888cb9fd63ec518e6a649270405
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-01-25 13:32:50 -0600

    test code

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -29,69 +29,85 @@ PageFrameAllocator frameAlloc;
 #if defined(__cplusplus)
 extern "C" {/* Use C linkage for kernel_main. */
 #endif
-	void kernelMain(multiboot_info_t* mbd) {
-		//turn off interrupts before configed
-		asm("cli");
-		//clear screen, set the pointer to the start of the VGA buffer.
-		terminalInit((uint16_t*) 0xC00B8000);
-		// Green on black!
-		terminalSetColor(vgaEntryColor(VGA_COLOR_GREEN, VGA_COLOR_BLACK));
-        terminalWriteLine("Terminal active, Welcome to ExOS!");
-        terminalWriteLine("Preparing your system:");
-        terminalWriteString("  Preparing GDT...");
-		//build and replce grub's GDT with a custom writable one.
-		gdt.build( );
-		gdt.load( );
-        terminalWriteLine(" Done!");
-        terminalWriteString("  Preparing the page table...");
-		//rebuild page table. replaces the one built in boot.s
-		pageTable.build( );
-        terminalWriteLine(" Done!");
-        terminalWriteString("  Setting up interrupts...");
-		//sets up interrups and enables them
-		interruptSetUp( );
-        terminalWriteLine(" Done!");
-        terminalWriteString("  Preparing the memory allocator...");
-		//get the memory map from grub
-		getMemMap(mbd);
-		//build the page frame allocator (allocate physical memory)
-		frameAlloc.build( );
-		//set malloc's support vars
-		mallocInit( );
-        terminalWriteLine(" Done!");
-        terminalWriteString("  Finding PCI buses...");
-		//find the valid PCI buses
-		PCIInit();
-        terminalWriteLine(" Done!");
-        terminalWriteString("  Finding USB host controllers...");
-		//find the (3 or less) USB host controllers. all have the same class/subclass code.
-		PCIDeviceList usbHostControllers(0x0C, 0x03, false);
-        terminalWriteLine(" Done!");
-        terminalWriteLine("!!!!ExOS fully booted!!!!");
-        printf("Test printf %d %c\n", 42, 'B');
-        printf("Newline is 0x%x%c", '\n', '\n');
-        for (int cx = 0; cx < 10; cx++) {
-            printf("%d", cx);
-        }
-		//dont return.
-		while (true) {
-			asm("hlt");
-		}
+
+void kernelMain(multiboot_info_t *mbd)
+{
+	//turn off interrupts before configed
+	asm("cli");
+	//clear screen, set the pointer to the start of the VGA buffer.
+	terminalInit((uint16_t *) 0xC00B8000);
+	// Green on black!
+	terminalSetColor(vgaEntryColor(VGA_COLOR_GREEN, VGA_COLOR_BLACK));
+	terminalWriteLine("Terminal active, Welcome to ExOS!");
+	terminalWriteLine("Preparing your system:");
+	terminalWriteString("  Preparing GDT...");
+	//build and replce grub's GDT with a custom writable one.
+	gdt.build();
+	gdt.load();
+	terminalWriteLine(" Done!");
+	terminalWriteString("  Preparing the page table...");
+	//rebuild page table. replaces the one built in boot.s
+	pageTable.build();
+	terminalWriteLine(" Done!");
+	terminalWriteString("  Setting up interrupts...");
+	//sets up interrups and enables them
+	interruptSetUp();
+	terminalWriteLine(" Done!");
+	terminalWriteString("  Preparing the memory allocator...");
+	//get the memory map from grub
+	getMemMap(mbd);
+	//build the page frame allocator (allocate physical memory)
+	frameAlloc.build();
+	//set malloc's support vars
+	mallocInit();
+	terminalWriteLine(" Done!");
+	terminalWriteString("  Finding PCI buses...");
+	//find the valid PCI buses
+	PCIInit();
+	terminalWriteLine(" Done!");
+	terminalWriteString("  Finding USB host controllers...");
+	//find the (3 or less) USB host controllers. all have the same class/subclass code.
+	PCIDeviceList usbHostControllers(0x0C, 0x03, false);
+	terminalWriteLine(" Done!");
+	terminalWriteLine("!!!!ExOS fully booted!!!!");
+	printf("Test printf %d %c\n", 42, 'B');
+	printf("Newline is 0x%x%c", '\n', '\n');
+	uint32_t i = (uint32_t) malloc(10);
+	printf("malloc returned 0x%x\n", i);
+	i = (uint32_t)malloc(100);
+	printf("malloc returned 0x%x\n", i);
+	 i =(uint32_t) malloc(50);
+	printf("malloc returned 0x%x\n", i);
+	 i = (uint32_t)malloc(1);
+	printf("malloc returned 0x%x\n", i);
+	printf("freeing 1 byte");
+	free((void*)i);
+	 i = (uint32_t)malloc(1);
+	printf("malloc returned 0x%x\n", i);
+/*
+	for (int cx = 0; cx < 10; cx++) {
+		printf("%d", cx);
+	}*/
+	//dont return.
+	while (true) {
+		asm("hlt");
 	}
+}
 
 #if defined(__cplusplus)
 }
 #endif
 
-void interruptSetUp( ) {
+void interruptSetUp()
+{
 	//build the IDT.
-	idt.build( );
+	idt.build();
 	//set the interrupt lines for the PIC
 	PICRemap(0x20, 0x28);
 	//mask the timer line
 	IRQSetMask(0);
 	//load it to the CPU
-	idt.load( );
+	idt.load();
 	//enable interupts
 	asm("sti");
 }
