760a2bce7dfe287ebcfc9a1e4d44842ac368c36f
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-01-27 14:04:59 -0600

    forloop i forgot

--- src/memory/alloc/Malloc.c
+++ src/memory/alloc/Malloc.c
@@ -40,25 +40,28 @@ extern "C" {/* Use C linkage for kernel_main. */
 void *malloc(size_t size)
 {
 	//TODO for loop
-	memHeaer* c = nullptr;
-	if(c->next == nullptr){
-		if(c-top >= (size + sizeof(memHeader)) && !c->used){
+	memHeaer* c = &kernelEnd;
+	for (; c != nullptr; c = c->next) {
+		if (c->next == nullptr) {
+			if (c - top >= (size + sizeof(memHeader)) && !c->used) {
+				//TODO alloc
+				//TODO new header
+				//TODO return
+			} else {
+				//TODO new page frame
+			}
+		} else if (!c->used && (c->next - c) == size) {
+			//TODO alloc
+			//TODO return
+		} else if (!c->used && (c->next - c) >= (size + sizeof(memHeader))) {
 			//TODO alloc
 			//TODO new header
 			//TODO return
 		} else {
-			//TODO new page frame
+			//TODO continue
 		}
-	} else if(!c->used && (c->next - c) == size ){
-		//TODO alloc
-		//TODO return
-	} else if (!c->used && (c->next - c) >= (size + sizeof(memHeader))){
-		//TODO alloc
-		//TODO new header
-		//TODO return
-	} else {
-		//TODO continue
 	}
+	
 	/*size += sizeof(memHeader);
 	//size_t space = (size_t) top - (size_t) & kernelEnd;
 	//if (space > size) {
