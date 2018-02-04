741f7874df9ba7de274e9591db43262265d214fe
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-02-03 22:46:57 -0600

    forgot to pack structs

--- src/drivers/filesys/fat/FatStructs.h
+++ src/drivers/filesys/fat/FatStructs.h
@@ -11,13 +11,13 @@ struct FatTime{
 	uint8_t hour:5;
 	uint8_t min:6;
 	uint8_t second:5;
-};
+} __attribute__((__packed__));
 
 struct FatDay{
 	uint8_t year:7;
 	uint8_t month:4;
 	uint8_t day:5;
-};
+}__attribute__((__packed__));
 
 struct FatBPB{
 	uint8_t jmp[3];
@@ -34,7 +34,7 @@ struct FatBPB{
 	uint16_t heads;
 	uint32_t hiddenSec;
 	uint32_t largeSecCount;
-};
+}__attribute__((__packed__));
 
 struct FatNormalFileName {
 	uint8_t name[11];
@@ -49,7 +49,7 @@ struct FatNormalFileName {
 	FatDay modDay;
 	uint16_t lowClusterNumber;
 	uint32_t size;
-};
+}__attribute__((__packed__));
 
 struct FatLongFileName {
 	uint8_t order;
@@ -60,6 +60,6 @@ struct FatLongFileName {
 	uint16_t nameMid[5];
 	uint16_t zero;
 	uint16_t nameEnd[2];
-};
+}__attribute__((__packed__));
 
 #endif //EXOS_FATSTRUCTS_H
