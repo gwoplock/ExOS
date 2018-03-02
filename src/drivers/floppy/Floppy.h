040e09cd7aa78a5c1113810c4655f9d20f7a7e76
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-02 16:42:39 -0600

    Revert "Added some more floppy constants"
    This reverts commit a762edbe158ff00770ff21ba7620450ec5643594.

--- src/drivers/floppy/Floppy.h
+++ src/drivers/floppy/Floppy.h
@@ -52,65 +52,6 @@ const uint16_t DIGITAL_INPUT_REGISTER = 0x3F7;
 const uint16_t CONFIGURATION_CONTROL_REGISTER = 0x3F7;
 
 /**
- * OR the values together for a DOR command
- * DOR format:
- *  Bits 0-1:
- *      00 - Drive 0
- *      01 - Drive 1
- *      10 - Drive 2
- *      11 - Drive 3
- *  Bit 2:
- *      0 - Reset controller
- *      1 - Controller enabled
- *  Bit 3:
- *      0 - IRQ channel
- *      1 - DMA mode
- *  Bits 4-7 (bit 4, drive 0 -> bit 7, drive 3):
- *      0 - Stop motor
- *      1 - Start motor
- */
-const uint8_t DOR_DRIVE0 = 0x00;    // 0000 00|00|
-const uint8_t DOR_DRIVE1 = 0x01;    // 0000 00|01|
-const uint8_t DOR_DRIVE2 = 0x02;    // 0000 00|10|
-const uint8_t DOR_DRIVE3 = 0x03;    // 0000 00|11|
-const uint8_t DOR_ENABLE = 0x04;    // 0000 0|1|00
-const uint8_t DOR_DMA    = 0x08;    // 0000 |1|000
-const uint8_t DOR_MOTOR0 = 0x10;    // 000|1| 0000
-const uint8_t DOR_MOTOR1 = 0x20;    // 00|1|0 0000
-const uint8_t DOR_MOTOR2 = 0x40;    // 0|1|00 0000
-const uint8_t DOR_MOTOR3 = 0x80;    // |1|000 0000
-
-/**
- * MSR format:
- *  Bit 0 Drive0
- *  Bit 1 Drive1
- *  Bit 2 Drive2
- *  Bit 3 Drive3:
- *      0 - Not busy in seek mode
- *      1 - Busy
- *  Bit 4:
- *      0 - FDC not busy
- *      1 - FDC busy with read/write
- *  Bit 5:
- *      0 - FDC in DMA mode
- *      1 - FDC in non-DMA mode
- *  Bit 6 (direction of transfer):
- *      0 - Expecting data from CPU
- *      1 - Has data for CPU
- *  Bit 7:
- *      0 - Data register not ready for transfer
- *      1 - Data register ready
- */
-const uint8_t MSR_DRIVE0_SEEK = 0x01;   // 0000 000|1|
-const uint8_t MSR_DRIVE1_SEEK = 0x02;   // 0000 00|1|0
-const uint8_t MSR_DRIVE2_SEEK = 0x04;   // 0000 0|1|00
-const uint8_t MSR_DRIVE3_SEEK = 0x08;   // 0000 |1|000
-const uint8_8 MSR_FDC_BUSY    = 0x10;   // 000|1| 0000
-const uint8_t MSR_FDC_DMA     = 0x20;   // 00|1|0 0000
-const uint8_t MSR_DIRECTION   = 0x40;   // 0|1|00 0000
-const uint8_t MSR_DATA_READY  = 0x80;   // |1|000 0000
-
-/**
  * Floppy commands
  */
 const uint8_t READ_TRACK = 2;           //   Generates IRQ6
