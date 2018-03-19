efdaf25385928e590fefb365336f1ccba869362f
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-19 10:24:10 -0500

    basic structure for usb device constructor

---
+++ src/drivers/PCI/USB/USBFunction.h
@@ -0,0 +1,21 @@
+#ifndef USB_Function_H_
+#define USB_FUNCTION_H_
+
+#include "drivers/generic/Device.h"
+
+//TODO: we need a USB base class, to be made as I learn more
+class USBFunction :public Device{
+    protected:
+    USBConfiguration** _configs;
+    size_t _currnetConfig;
+    uint8_t _address;
+    //TODO check size
+    int _maxDataSize;
+    USBHostController _host;
+    public:
+    USBFunction();
+    USBFunction(USBHub hub);
+
+};
+
+#endif
