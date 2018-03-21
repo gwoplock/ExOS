dfd0e13f9b7cd1b9509bc4299e4ede8b69a02dab
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-21 08:50:51 -0500

    start of EHCI root hub

--- src/drivers/PCI/USB/USB_hubs/USBHub.h
+++ src/drivers/PCI/USB/USB_hubs/USBHub.h
@@ -3,9 +3,19 @@
 
 
 #include "Global.h"
-
+#include "drivers/PCI/USB/hostController/USBHostController.h"
 //TODO superclass
 class USBHub{
+    protected:
+        //TODO size
+        int _portCount;
+        USBHostController* _host; 
+        USBHub(){
+
+        }
+        USBHub(USBHostController* host):_host(host){
+            //TODO port count
+        }
 
 };
 
